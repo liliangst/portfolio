fe1d54dc56073e30f9703ec83b48888dae94868a
liliangst <liliangst@outlook.fr>
2024-03-07 15:22:56 +0100

    Adding Plot library to the project

--- Package.swift
+++ Package.swift
@@ -10,13 +10,15 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
+        .package(url: "https://github.com/johnsundell/plot.git", from: "0.9.0"),
     ],
     targets: [
         .target(
             name: "App",
             dependencies: [
                 .product(name: "Leaf", package: "leaf"),
-                .product(name: "Vapor", package: "vapor")
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Plot", package: "plot")
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
